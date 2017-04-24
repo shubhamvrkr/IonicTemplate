c22152b9c52fbeade4388f82eef3d9074de771d7
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-04-24 15:14:19 +0530

    Capture user password to decrypt file

--- Frontend/MicroLending/www/js/controllers/loginController.js
+++ Frontend/MicroLending/www/js/controllers/loginController.js
@@ -1,18 +1,18 @@
-mycontrollerModule.controller('loginCtrl', ['$scope', '$stateParams', '$state', '$ionicLoading', '$timeout', 'fileFactory', 'loginFactory', "$cordovaZip", 'registerFactory', '$ionicPush', 'ionicToast', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+mycontrollerModule.controller('loginCtrl', ['$scope', '$stateParams', '$state', '$ionicLoading', '$timeout', 'fileFactory', 'loginFactory', "$cordovaZip", 'registerFactory', '$ionicPush', 'ionicToast', '$ionicPopup', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
   // You can include any angular dependencies as parameters for this function
   // TIP: Access Route Parameters for your page via $stateParams.parameterName
-  function ($scope, $stateParams, $state, $ionicLoading, $timeout, fileFactory, loginFactory, $cordovaZip, registerFactory, $ionicPush, ionicToast) {
+  function($scope, $stateParams, $state, $ionicLoading, $timeout, fileFactory, loginFactory, $cordovaZip, registerFactory, $ionicPush, ionicToast, $ionicPopup) {
 
 
-    $scope.Login = function (data) {
+    $scope.Login = function(data) {
 
-      console.log("Login")
-      var user_name = data.username
-      var password = data.password
-      var login_data = {}
+      console.log("Login");
+      var user_name = data.username;
+      var password = data.password;
+      var login_data = {};
 
-      login_data.username = data.username
-      login_data.password = data.password
+      login_data.username = data.username;
+      login_data.password = data.password;
 
       console.log(login_data);
 
@@ -25,96 +25,97 @@ mycontrollerModule.controller('loginCtrl', ['$scope', '$stateParams', '$state', 
       });
 
       if (window.cordova) {
-        // ss - Secure Storage
         ss.get(
-          function (value) {
+          function(value) {
             console.log('Success, got ' + value);
             login_data.ks = JSON.parse(value).ks;
             console.log(login_data.ks);
             //check for the email validation
 
             if (login_data.username == JSON.parse(value).email || login_data.username == JSON.parse(value).address) {
-              loginFactory.login(login_data, function (err, result) {
+              loginFactory.login(login_data, function(err, result) {
                 if (err) {
                   $ionicLoading.hide();
                   $scope.error = err;
-                  console.log(err)
+                  console.error(err);
                   //console.log($scope.error)
-                  return
+                  return;
                 } else {
                   $ionicLoading.hide();
-                  console.log("Login", result)
+                  console.log("Login", result);
                   $state.go('menu.allContracts');
                 }
-              })
+              });
             } else {
               $ionicLoading.hide();
               $scope.error = "Incorrect username or password";
-              console.log("incorrect email or address")
-              return
+              console.log("incorrect email or address");
+              return;
             }
           },
-          function (error) {
+          function(error) {
             console.log('Error ' + error);
           },
           'user_data');
-
       } else {
-        value = JSON.parse(localStorage.getItem('user_data'))
-        login_data.ks = value.ks
+        value = JSON.parse(localStorage.getItem('user_data'));
+        login_data.ks = value.ks;
         console.log(value);
         //check for the email validation
-        console.log(value.email)
-        console.log(login_data.username == value.email)
+        console.log(value.email);
+        console.log(login_data.username == value.email);
         if (login_data.username == value.email || login_data.username == value.address) {
-          loginFactory.login(login_data, function (err, result) {
+
+          loginFactory.login(login_data, function(err, result) {
             if (err) {
               $scope.error = err;
-              console.log("Login error", err)
-            }
-            else {
-              console.log("Login", result)
+              console.log("Login error", err);
+            } else {
+              console.log("Login", result);
+              // save the pwderived key in SS.
               $state.go('menu.allContracts');
             }
             $ionicLoading.hide();
           });
+
         } else {
           $ionicLoading.hide();
           $scope.error = "Incorrect username or password";
-          console.log("incorrect email or address")
-          return
+          console.log("incorrect email or address");
+          return;
         }
       }
-    }
+    };
 
-    $scope.onchangeinput = function () {
+    $scope.onchangeinput = function() {
       $scope.error = "";
-    }
+    };
+    $scope.Import = function() {
 
-    $scope.Import = function () {
       if (window.cordova) {
         console.log("Device");
         fileChooser.open(success, error);
+
       } else {
-        console.log("Browser")
-        console.log()
+        console.log("Browser");
+        console.log();
         var input = document.getElementById("fileLoader");
         input.click();
       }
-    }
-
-    var success = function (data) {
+    };
+    var success = function(data) {
       console.log("new Data: ", data);
       var permissions = cordova.plugins.permissions;
-      console.log(permissions)
+      console.log(permissions);
       permissions.hasPermission(permissions.READ_EXTERNAL_STORAGE, checkPermissionCallback, null);
 
       function checkPermissionCallback(status) {
-        console.log("status: ", status)
+        console.log("status: ", status);
+
         if (!status.hasPermission) {
           permissions.requestPermission(
             permissions.READ_EXTERNAL_STORAGE,
-            function (status) {
+            function(status) {
               if (!status.hasPermission) {
                 errorCallback();
               } else {
@@ -123,14 +124,13 @@ mycontrollerModule.controller('loginCtrl', ['$scope', '$stateParams', '$state', 
               }
             }, errorCallback);
         } else {
-          resolveNativePath()
+          resolveNativePath();
         }
       }
 
       function resolveNativePath() {
-        window.FilePath.resolveNativePath(data, function (filepath) {
+        window.FilePath.resolveNativePath(data, function(filepath) {
           console.log("File path: ", filepath);
-
           //create a directory micro_lending
           $ionicLoading.show({
             templateUrl: 'templates/loading.html',
@@ -139,60 +139,73 @@ mycontrollerModule.controller('loginCtrl', ['$scope', '$stateParams', '$state', 
             maxWidth: 200,
             showDelay: 0
           });
-
-          fileFactory.createDirectory("micro_lending", "/", function (res) {
-
-            console.log("App creation", res)
-
+          fileFactory.createDirectory("micro_lending", "/", function(res) {
+            console.log("App creation", res);
             //handle unzipping here for android
-            fileFactory.unZip("", filepath, function (data) {
-
+            fileFactory.unZip("", filepath, function(data) {
               console.log("unzip status", data);
               if (data.status == "0") {
                 $ionicLoading.hide();
                 ionicToast.show('Please upload valid zip file', 'bottom', false, 2500);
               }
-
-              fileFactory.readFile("user_profile.json", "micro_lending/user_data/", function (data) {
+              fileFactory.readFile("user_profile.json", "micro_lending/user_data/", function(data) {
 
                 if (data.status == "0") {
-
                   console.log("Error reading file after zipping");
                   $ionicLoading.hide();
                   ionicToast.show('Please upload valid zip file', 'bottom', false, 2500);
+                } else {
+                  console.log(data);
+                  //take password and try to decrypt the key-store. call login service module
+                  var login_data = {};
+                  login_data.username = JSON.parse(data.data).email;
 
+                  $ionicPopup.prompt({
+                    title: 'Password',
+                    subTitle: 'Enter your password to decrypt file',
+                    inputType: 'password',
+                    inputPlaceholder: 'Your password'
+                  }).then((pass) => {
+                    if (!pass) {
+                      console.error("Password not entered");
+                      ionicToast.show('You need to enter password!', 'bottom', false, 2500);
+                      return;
+                    }
+                    loginFactory.login(login_data, function(err, result) {
+                      if (err) {
+                        console.error(err);
+                      } else {
+                        // +save the pwderived key in SS.
+                        registerFactory.saveUserDataLocally(data.data, 'user_data', function(res) {
+                          console.log(res);
+                          $ionicLoading.hide();
+                          ionicToast.show('Profile successfully imported', 'bottom', false, 2500);
+                          $state.go('menu.allContracts');
+                        });
+                      }
+                    });
+                  });
                 }
-
-                console.log(data)
-                registerFactory.saveUserDataLocally(data.data, 'user_data', function (res) {
-                  console.log(res);
-                  $ionicLoading.hide();
-                  ionicToast.show('Profile successfully imported', 'bottom', false, 2500);
-                  $state.go('menu.allContracts');
-                });
-              })
-            })
-          })
-
-        }, function (code, message) {
+              });
+            });
+          });
+        }, function(code, message) {
           console.log(code);
           console.log(message);
-
         });
       }
     };
 
-    var error = function (msg) {
-      console.log("error: ", msg);
-    };
+    var error = function(msg) {
+      console.error("error: ", msg);
 
-    var errorCallback = function () {
+    };
+    var errorCallback = function() {
       //user didnt grant permission
       //tell him export couldnot be done
       console.warn('Storage permission is not turned on');
-    }
-
-    $scope.fileNameChanged = function (element) {
+    };
+    $scope.fileNameChanged = function(element) {
       //browser
       $ionicLoading.show({
         templateUrl: 'templates/loading.html',
@@ -202,18 +215,46 @@ mycontrollerModule.controller('loginCtrl', ['$scope', '$stateParams', '$state', 
         showDelay: 0
       });
 
-      fileFactory.unZip("", element.files[0], function (data) {
+      fileFactory.unZip("", element.files[0], function(data) {
+        //take password and then call login service to check if the password is correct or wrong. IF password is incorrect - display incorrect key-store
+
+        var login_data = {};
         if (data.status == "0") {
           $ionicLoading.hide();
           ionicToast.show('Please upload valid zip file', 'bottom', false, 2500);
         } else {
-          registerFactory.saveUserDataLocally(JSON.stringify(data.data), 'user_data', function (res) {
-            $ionicLoading.hide();
-            console.log("saved in local Storage", res)
-            ionicToast.show('Profile successfully imported', 'bottom', false, 2500);
-            $state.go('menu.allContracts');
+          login_data.username = JSON.parse(data.data).email;
+
+          $ionicPopup.prompt({
+            title: 'Password',
+            subTitle: 'Enter your password to decrypt file',
+            inputType: 'password',
+            inputPlaceholder: 'Your password'
+          }).then((pass) => {
+            if (!pass) {
+              console.error("Password not entered");
+              ionicToast.show('You need to enter password!', 'bottom', false, 2500);
+              return;
+            }
+
+            login_data.password = pass;
+            loginFactory.login(login_data, function(err, result) {
+              if (err) {
+                console.error(err);
+              } else {
+                // +save the pwderived key in secure storage
+                registerFactory.saveUserDataLocally(JSON.stringify(data.data), 'user_data', function(res) {
+
+                  $ionicLoading.hide();
+                  console.log("saved in local Storage", res);
+                  ionicToast.show('Profile successfully imported', 'bottom', false, 2500);
+                  $state.go('menu.allContracts');
+                });
+              }
+            });
           });
         }
-      })
-    }
-  }]);
+      });
+    };
+  }
+]);
