89e859a58641eb5cefbc299569cbaf5d515d6ce4
Amol Pednekar <amol0202@gmail.com>
2017-04-26 15:44:47 +0530

    Button loader | Ionic FCM token to DB

--- Frontend/MicroLending/www/js/controllers/createcontractController.js
+++ Frontend/MicroLending/www/js/controllers/createcontractController.js
@@ -1,8 +1,18 @@
-mycontrollerModule.controller('createDealCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$state) {
+mycontrollerModule.controller('createDealCtrl', ['$scope', '$stateParams', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  // You can include any angular dependencies as parameters for this function
+  // TIP: Access Route Parameters for your page via $stateParams.parameterName
+  function ($scope, $stateParams, $state) {
 
+    $scope.spinnerFlag = true;
+    $scope.textFlag = false;
+    $scope.CreateDeal = function (data) {
 
-}])
- 
+      $scope.spinnerFlag = false;
+      $scope.textFlag = true;
+    }
+
+    // On success
+    $scope.spinnerFlag = true;
+    $scope.textFlag = false;
+
+  }])
