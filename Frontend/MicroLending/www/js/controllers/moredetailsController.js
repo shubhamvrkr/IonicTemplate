883095cebd80542bf323b525c4b754d2ceed3831
Nitesh Solanki <nits7sid@gmail.com>
2017-05-03 18:53:23 +0530

    add email in tx_array object

--- Frontend/MicroLending/www/js/controllers/moredetailsController.js
+++ Frontend/MicroLending/www/js/controllers/moredetailsController.js
@@ -1,105 +1,127 @@
-mycontrollerModule.controller('moredetailsCtrl', ['$scope', '$stateParams', '$state', '$ionicLoading', '$timeout', '$ionicHistory','databaseFactory',
-  function ($scope, $stateParams, $state, $ionicLoading, $timeout, $ionicHistory,databaseFactory) {
+mycontrollerModule.controller('moredetailsCtrl', ['$scope', '$stateParams', '$state', '$ionicLoading', '$timeout', '$ionicHistory', 'databaseFactory', 'getCurrentUserData',
+  function ($scope, $stateParams, $state, $ionicLoading, $timeout, $ionicHistory, databaseFactory, getCurrentUserData) {
 
-    console.log("more details")
+    console.log("more details");
+    //load current user details
 
-    if ($stateParams.contract==null){
+    if ($stateParams.contract == null) {
       console.log("NUL")
-       $timeout(function () {
+      $timeout(function () {
         $state.go('menu.allContracts');
-        }, 0);
-    }else{
+      }, 0);
+    } 
 
-    $scope.dealData = $stateParams.contract;
-    console.log($stateParams.contract);
-    console.log("Deal data: ", $scope.dealData);
+else{
+      getCurrentUserData.getData(function (currentUser) {
+        console.log("1111111111111")
+        $scope.currentUserEmail = currentUser.data.email;
+        $scope.currentUserAddress = currentUser.data.from_eth_address;
+ });
+}
 
-    // perform verfication of the signatures of all the tx's in $scope.dealData.tx
-    var tx_array = $scope.dealData.tx;
-    //get the event details from geth Client using tx_hash
+   $scope.dealData = $stateParams.contract;
+        console.log($stateParams.contract);
+        console.log("Deal data: ", $scope.dealData);
 
-    console.log(ethdapp.web3)
+        // perform verfication of the signatures of all the tx's in $scope.dealData.tx
+        var tx_array = $scope.dealData.tx;
+        //get the event details from geth Client using tx_hash
 
-    var data = ethdapp.web3.eth.getTransactionReceipt(tx_array[0].txHash);
+        console.log(tx_array)
+        for (var k = 0; k < tx_array.length; k++) {
 
-    var event_data = data.logs[0].data;
-    var log = data.logs[0];
+          if (tx_array[k].caller== $scope.currentUserEmail){
+            console.log("No counter party transactions found!");
+          }
 
-    var event = null;
+          else{
+          var data = ethdapp.web3.eth.getTransactionReceipt(tx_array[k].txHash);
+            
+          var event_data = data.logs[0].data;
+          var log = data.logs[0];
 
-    for (var i = 0; i < ABI.length; i++) {
-      var item = ABI[i];
-      if (item.type != "event") continue;
-      var signature = item.name + "(" + item.inputs.map(function (input) { return input.type; }).join(",") + ")";
-      var hash = ethdapp.web3.sha3(signature);
-      console.log(log.topics[0])
-      console.log(hash)
-      if ("0x" + hash == log.topics[0]) {
-        event = item;
-        break;
-      }
-    }
+          var event = null;
+
+          for (var i = 0; i < ABI.length; i++) {
+            var item = ABI[i];
+            if (item.type != "event") continue;
+            var signature = item.name + "(" + item.inputs.map(function (input) { return input.type; }).join(",") + ")";
+            var hash = ethdapp.web3.sha3(signature);
+            console.log(log.topics[0])
+            console.log(hash)
+            if ("0x" + hash == log.topics[0]) {
+              event = item;
+              break;
+            }
+          }
 
 
-    if (event != null) {
-      console.log(event)
-      var inputs = event.inputs.map(function (input) { return input.type; });
-      var data = solidity.decodeParams(inputs, log.data.replace("0x", ""));
-      console.log(JSON.parse(data[2]));
-      var sig_data = JSON.parse(data[2]);
-      var temp_contract_data = {};
+          if (event != null) {
+            console.log(event)
+            var inputs = event.inputs.map(function (input) { return input.type; });
+            var data = solidity.decodeParams(inputs, log.data.replace("0x", ""));
+            console.log(JSON.parse(data[2]));
+            var sig_data = JSON.parse(data[2]);
+            var temp_contract_data = {};
 
-      temp_contract_data.deal_id = $scope.dealData._id.toString();
-      temp_contract_data.from_ethAddress = $scope.dealData.from_address;
-      temp_contract_data.to_ethAddress = $scope.dealData.counter_party_address;
-      temp_contract_data.from_email = $scope.dealData.from_email;
-      temp_contract_data.to_email = $scope.dealData.counter_party_email;
-      temp_contract_data.start_date = $scope.dealData.start_date;
-      temp_contract_data.end_date = $scope.dealData.end_date;
-      temp_contract_data.asset_id = $scope.dealData.asset_id;
-      temp_contract_data.asset_name = $scope.dealData.asset_name;
-      temp_contract_data.description = $scope.dealData.description;
-      temp_contract_data.nonce = sig_data.nonce;
+            temp_contract_data.deal_id = $scope.dealData._id.toString();
+            temp_contract_data.from_ethAddress = $scope.dealData.from_address;
+            temp_contract_data.to_ethAddress = $scope.dealData.counter_party_address;
+            temp_contract_data.from_email = $scope.dealData.from_email;
+            temp_contract_data.to_email = $scope.dealData.counter_party_email;
+            temp_contract_data.start_date = $scope.dealData.start_date;
+            temp_contract_data.end_date = $scope.dealData.end_date;
+            temp_contract_data.asset_id = $scope.dealData.asset_id;
+            temp_contract_data.asset_name = $scope.dealData.asset_name;
+            temp_contract_data.description = $scope.dealData.description;
+            temp_contract_data.nonce = sig_data.nonce;
 
-      console.log(temp_contract_data)
+            console.log(temp_contract_data)
 
-      var s_hex = buffer.from(sig_data.sig_s.toString('hex'), 'hex');
-      console.log(s_hex)
-      var r_hex = buffer.from(sig_data.sig_r.toString('hex'), 'hex');
-      console.log(r_hex)
-      var v_hex = parseInt(sig_data.sig_v);
-      console.log(v_hex)
+            var s_hex = buffer.from(sig_data.sig_s.toString('hex'), 'hex');
+            console.log(s_hex)
+            var r_hex = buffer.from(sig_data.sig_r.toString('hex'), 'hex');
+            console.log(r_hex)
+            var v_hex = parseInt(sig_data.sig_v);
+            console.log(v_hex)
 
-      //get the from ethereum address
+            //get the from ethereum address
+            //if ($scope.currentUserAddress==tx_array[0].caller)
 
-      databaseFactory.getDocById(contact_db, tx_array[0].caller, function (response) {
 
-        if (response.status == "0") {
 
-          console.log("contact with email not found : ");
+            databaseFactory.getDocById(contact_db, tx_array[k].caller, function (response) {
+                  console.log("Caller",tx_array[k].caller)
+              if (response.status == "0") {
 
-        } else {
-            console.log(response)
-          EthWallet.encryption_sign.verifyMsg(response.eth_address, JSON.stringify(temp_contract_data), v_hex, r_hex, s_hex, function (err, verifiedResult) {
+                console.log("contact with email not found : ");
 
-            if (err) {
+              } else {
 
-              console.log("Error in verifying signature: ", err);
+                console.log(response)
+                EthWallet.encryption_sign.verifyMsg(response.eth_address, JSON.stringify(temp_contract_data), v_hex, r_hex, s_hex, function (err, verifiedResult) {
 
-            } else {
+                  if (err) {
 
-              console.log("Verification Status: ", verifiedResult);
+                    console.log("Error in verifying signature: ", err);
 
-            };
+                  } else {
 
-          });
+                    console.log("Verification Status: ", verifiedResult);
+
+                  };
+
+                });
+              }
+            });
+
+          }
         }
-      });
-    }
+        }
+     
+     //console.log(ethdapp.web3.toAscii(event_data));
 
-  }  //console.log(ethdapp.web3.toAscii(event_data));
-    
-  $scope.backButtonPress = function () {
+    $scope.backButtonPress = function () {
 
       console.log("back button pressed")
       $ionicHistory.goBack();
