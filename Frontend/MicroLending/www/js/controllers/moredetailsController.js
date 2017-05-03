95568d0a7a7b3da1d3cef5484eb649313b9626ef
Nitesh Solanki <nits7sid@gmail.com>
2017-05-03 14:44:59 +0530

    added object field in tx_array

--- Frontend/MicroLending/www/js/controllers/moredetailsController.js
+++ Frontend/MicroLending/www/js/controllers/moredetailsController.js
@@ -5,7 +5,17 @@ mycontrollerModule.controller('moredetailsCtrl', ['$scope', '$stateParams', '$st
 
     $scope.dealData = $stateParams.contract;
     console.log($stateParams.contract);
-    console.log($scope.dealData);
+    console.log("Deal data: ",$scope.dealData);
+
+   // perform verfication of the signatures of all the tx's in $scope.dealData.tx
+   var tx_array = $scope.dealData.tx;
+   //get the event details from geth Client using tx_hash
+
+   console.log(ethdapp.web3 )
+   
+   var data = ethdapp.web3.eth.getTransactionReceipt(tx_array[0].txHash);
+
+   console.log(data)
     $scope.backButtonPress = function () {
 
       console.log("back button pressed")
