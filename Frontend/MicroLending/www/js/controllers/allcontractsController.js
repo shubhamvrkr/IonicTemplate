701ad74c7fe7da7e444dbd33c8d9de7869c63cdb
Dinesh Rivankar <dinesh_rivankar@persistent.com>
2017-05-09 14:10:54 +0530

    Accordion

--- Frontend/MicroLending/www/js/controllers/allcontractsController.js
+++ Frontend/MicroLending/www/js/controllers/allcontractsController.js
@@ -475,4 +475,34 @@ mycontrollerModule.controller('allContractsCtrl', ['$scope', '$stateParams', '$s
 
     }
 
+	
+	
+	
+	
+	
+	
+		$scope.groups = [
+						{ name: "Pending Contracts",items:$scope.pendingcontracts},
+						{ name: "Active Contracts",items:[1,2,3]},
+						{ name: "Rejected Contracts",items:[1,2,3]},
+						{ name: "Completed Contracts",items:[1,2,3]}
+					];
+					
+		
+  
+	  /*
+	   * if given group is the selected group, deselect it
+	   * else, select the given group
+	   */
+	  $scope.toggleGroup = function(group) {
+		if ($scope.isGroupShown(group)) {
+		  $scope.shownGroup = null;
+		} else {
+		  $scope.shownGroup = group;
+		}
+	  };
+	  $scope.isGroupShown = function(group) {
+		return $scope.shownGroup === group;
+	  };
+	  
   }]);
