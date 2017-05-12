636128c6262542197d3c878aa2faa8b059ca5377
Nitesh Solanki <nits7sid@gmail.com>
2017-05-12 12:29:47 +0530

    open frst default accordian

--- Frontend/MicroLending/www/js/controllers/allcontractsController.js
+++ Frontend/MicroLending/www/js/controllers/allcontractsController.js
@@ -557,7 +557,7 @@ mycontrollerModule.controller('allContractsCtrl', ['$scope', '$stateParams', '$s
         $scope.unsettledcontracts = response;
         $scope.$apply();
         $scope.groups.push({ name: "Contracts Due for settlement", items: [], notification_flag: "false", notification_count: 0, isExpired: "false" });
-        $scope.shownGroup = $scope.groups[0];
+        //$scope.shownGroup = $scope.groups[0];
         $scope.unsettledcontracts.forEach(function (element) {
 
           if (element.tx.length != 1) {
@@ -582,9 +582,9 @@ mycontrollerModule.controller('allContractsCtrl', ['$scope', '$stateParams', '$s
          tem = $scope.groups[2];
         $scope.groups[2] = $scope.groups[1];
         $scope.groups[1] = tem;
-        $scope.shownGroup = $scope.groups[0];
+       // $scope.shownGroup = $scope.groups[0];
         $scope.$apply();
-
+checkAccordianIsOpen();
     
 
         console.log("Length is ", $scope.groups);
@@ -628,5 +628,25 @@ mycontrollerModule.controller('allContractsCtrl', ['$scope', '$stateParams', '$s
 		$scope.$broadcast('scroll.refreshComplete');
 	
 	}
+
+  function checkAccordianIsOpen(){
+    if($scope.groups[0].items.length!=0){
+      $scope.shownGroup = $scope.groups[0];
+
+    }else if($scope.groups[1].items.length!=0){
+      $scope.shownGroup = $scope.groups[1];
+
+    
+    }else if($scope.groups[2].items.length!=0){
+      $scope.shownGroup = $scope.groups[2];
+
+    
+    }else if($scope.groups[3].items.length!=0){
+      $scope.shownGroup = $scope.groups[3];
+
+    
+    }
+
+  }
 
   }]);
